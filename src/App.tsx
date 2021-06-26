89a9c0a563bcf01ca821b68dd9da9b25b5ec7cf9
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2021-06-26 00:20:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
+function App() {
+  return (
+    <BrowserRouter>
+        <AuthContextProvider>
+            <Route path="/" exact component={Home}/>
+            <Route path="/rooms/new" component={NewRoom}/>
+        </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+// 1 dia - together
