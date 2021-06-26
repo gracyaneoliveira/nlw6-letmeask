e47dcff3772f3d56ace335b8671288aef6c2f90e
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2021-06-26 19:08:59 -0300

    Create Room

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -10,13 +10,14 @@ function App() {
   return (
     <BrowserRouter>
         <AuthContextProvider>
-            <Route path="/" exact component={Home}/>
-            <Route path="/rooms/new" component={NewRoom}/>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/rooms/new" component={NewRoom}/>
+                <Route path="/rooms/:id" component={Room}/>
+            </Switch>
         </AuthContextProvider>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-// 1 dia - together
